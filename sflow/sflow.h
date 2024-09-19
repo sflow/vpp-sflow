85754f0badddb1fdb2e7b10cb70eed7c2f6f044f
Neil McKee <neil.mckee@inmon.com>
2024-09-19 16:51:42 -0700

    copyright notices

--- sflow/sflow.h
+++ sflow/sflow.h
@@ -1,8 +1,5 @@
-
 /*
- * sflow.h - sflow plug-in header file
- *
- * Copyright (c) <current-year> <your-organization>
+ * Copyright (c) 2024 InMon Corp.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at:
